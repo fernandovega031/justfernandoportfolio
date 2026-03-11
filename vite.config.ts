d8e85b12ddab586ea4f18033e0ad918a628fca49
Fernando Vega <51734133+fernandovega031@users.noreply.github.com>
2026-03-11 10:48:04 -0600

    add gh-pages and custom files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://www.justfernandosportfolio.com',
+  assetsInclude: ['**/*.JPG'],
   plugins: [react()],
   resolve: {
     alias: {
